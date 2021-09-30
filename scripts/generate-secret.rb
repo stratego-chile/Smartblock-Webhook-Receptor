28747ef2ac409294fe725fd2ea7c2eb7b398c21d
Benjamin Calisto <theuserbit@protonmail.com>
2021-09-30 13:04:56 -0300

    feat: add --repeat flag to secret generator

--- scripts/generate-secret.rb
+++ scripts/generate-secret.rb
@@ -1,15 +1,38 @@
 require 'securerandom'
 require 'optparse'
 
+DEFAULT_LENGTH = 20
+DEFAULT_REPEAT = 1
+
 begin
   options = { }
-  DEFAULT_LENGTH = 20
+
+  length = DEFAULT_LENGTH
+  repeat = DEFAULT_REPEAT
+
   OptionParser.new do |opt|
     opt.on('--length LENGTH') { |o| options[:length] = o } # Get `--length` flag value
+    opt.on('--repeat REPEAT') { |o| options[:repeat] = o } # Get `--repeat` flag value
   end.parse!
-  inline_length = Integer(options[:length]) if options[:length]
-  length = (inline_length and inline_length > 0) ? inline_length : DEFAULT_LENGTH
-  puts SecureRandom.alphanumeric(length) # Generates a random string
+
+  if options[:length]
+    length_input = Integer(options[:length])
+    if length_input > 0
+      length = length_input
+    end
+  end
+
+  if options[:repeat]
+    repeat_input = Integer(options[:repeat])
+    if repeat_input > 1
+      repeat = repeat_input
+    end
+  end
+
+  while repeat > 0
+    puts SecureRandom.alphanumeric(length) # Generates a random string
+    repeat -= 1
+  end
 rescue => exception
   puts 'Process exit caused by an unexpected input:', exception
 end
