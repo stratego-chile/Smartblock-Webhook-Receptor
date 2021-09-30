9df3bb353a13c5170a4c0e91d8abf66b4524c524
Benjamin Calisto <theuserbit@protonmail.com>
2021-09-30 12:49:10 -0300

    fix: secret generator method type & input validation control

--- scripts/generate-secret.rb
+++ scripts/generate-secret.rb
@@ -3,11 +3,13 @@ require 'optparse'
 
 begin
   options = { }
+  DEFAULT_LENGTH = 20
   OptionParser.new do |opt|
     opt.on('--length LENGTH') { |o| options[:length] = o } # Get `--length` flag value
   end.parse!
   inline_length = Integer(options[:length]) if options[:length]
-  puts SecureRandom.hex(inline_length || 20) # Generate an hexadecimal based string
+  length = (inline_length and inline_length > 0) ? inline_length : DEFAULT_LENGTH
+  puts SecureRandom.alphanumeric(length) # Generates a random string
 rescue => exception
-  puts 'Process exit caused by an input error:', exception
+  puts 'Process exit caused by an unexpected input:', exception
 end
